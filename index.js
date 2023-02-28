e6b891737f64a7b19977a860c36956523bf79f06
Avantika tiwari <avantikatiwari18@gmail.com>
2023-02-28 22:22:14 +0530

    msg

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ app.use(express.json());
 
 app.post("/api/submituser",(req, res) => {
     try{
-        console.log(req.body);
-        console.log(req.body.name);
-        console.log(req.body.branch);
-        console.log(req.body.batch);
-        console.log(req.body.lastname);
+        // console.log(req.body);
+        // console.log(req.body.name);
+        // console.log(req.body.branch);
+        // console.log(req.body.batch);
+        // console.log(req.body.lastname);
+
+        const newobject ={
+            username : req.body.name,
+            userbatch : req.body.batch,
+            userbranch: req.body.branch,
+            userlastname: req.body.lastname,
+        };
+        console.log(newobject);
 
         return res.json({success:true, message: "testing submit api"})
 
