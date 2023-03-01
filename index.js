613a7d8618d8af8dd1615a44a8e44b4c0377a4d4
Avantika tiwari <avantikatiwari18@gmail.com>
2023-03-01 22:47:42 +0530

    msg

--- index.js
+++ index.js
@@ -4,22 +4,38 @@ const app = express();
 app.use(express.json());
 
 app.post("/api/submituser",(req, res) => {
+});
+app.post("/api/login" , (req, res) => {
     try{
+
+        let originalPassword = "avantikatiwari18@gmail.com";
+        let enteredPassword = "req.body.password";
+        if(originalPassword === enteredPassword) {
+            return res.status(200).json({ success: true, message: "logged in successfully"});
+
+        } else{
+            return res.status(401).json({ succes: false, message : "please check your password"});
+        }
+
+
+
+   
+
         // console.log(req.body);
         // console.log(req.body.name);
         // console.log(req.body.branch);
         // console.log(req.body.batch);
         // console.log(req.body.lastname);
 
-        const newobject ={
-            username : req.body.name,
-            userbatch : req.body.batch,
-            userbranch: req.body.branch,
-            userlastname: req.body.lastname,
-        };
-        console.log(newobject);
+        // const newobject ={
+        //     username : req.body.name,
+        //     userbatch : req.body.batch,
+        //     userbranch: req.body.branch,
+        //     userlastname: req.body.lastname,
+        // };
+        // console.log(newobject);
 
-        return res.json({success:true, message: "testing submit api"})
+        // return res.json({success:true, message: "testing submit api"})
 
     } catch(error) {
         return res.status(400).json({ success: false, error:error.message })
